410bbbec98b7d72cfd2434b49915f165060cffa8
Subham Panwar <subhampanwarr@gmail.com>
2026-08-03 11:00:17 +0000

    Backup existing EKS Terraform configuration

--- DevOps-Project-19/tf-aws-eks/backend.tf
+++ DevOps-Project-19/tf-aws-eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-eks-cicd-7001"
+    bucket = "terraform-eks-cicd-248847837354-us-east-1"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
